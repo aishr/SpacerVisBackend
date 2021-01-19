41f6ee522e19a7f254ea375dbf9ce99e11cdeccf
Nham Le <nv3le@uwaterloo.ca>
2021-01-19 14:46:14 -0500

    add 2 more schemas to DB

--- pobvis/app/reinit_db.sh
+++ pobvis/app/reinit_db.sh
@@ -4,3 +4,4 @@ rm -rf media
 mkdir media
 sqlite3 exp_db "create table exp(name varchar(100), done bool, result varchar(5), time int, aux varchar(200));"
 sqlite3 exp_db "create table expr_map(exp_path varchar(100), expr_id int, value text, PRIMARY KEY ( exp_path, expr_id));"
+sqlite3 exp_db "create table learned_programs(hash varchar(256), human_readable_ast text, xml_ast text, comment text, PRIMARY KEY(hash));"
